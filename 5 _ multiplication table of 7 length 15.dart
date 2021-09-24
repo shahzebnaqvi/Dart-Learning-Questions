5bba38acb2e21d88b296bd3d197f34a1bff8db94
shahzebnaqvi <50788207+shahzebnaqvi@users.noreply.github.com>
2021-09-24 10:08:09 +0500

    Question 5

---
+++ 5 _ multiplication table of 7 length 15.dart
@@ -0,0 +1,13 @@
+
+
+void main()
+{
+	var tableOf= 7;
+  var startTab = 1;
+  var endTab = 15;
+  print ("_____ Table of $tableOf _____");
+  print("----------------------");
+  for (var i=startTab; i<=endTab ; i++){
+    print (".... $tableOf X $i = ${tableOf * i} ....");
+  }
+}
